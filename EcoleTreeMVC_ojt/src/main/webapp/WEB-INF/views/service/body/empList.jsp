6404334d9942b243dbea2875bbed1b07f71ce60c
Jeongjin Kim <valorjj@gmail.com>
2024-01-03 16:42:50 +0900

    체크박스

--- EcoleTreeMVC_ojt/src/main/webapp/WEB-INF/views/service/body/empList.jsp
+++ EcoleTreeMVC_ojt/src/main/webapp/WEB-INF/views/service/body/empList.jsp
@@ -1,56 +1,58 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <div style="width:1200px;">
-	<h1> 사원관리 </h1> <br><br>
+    <h1> 사원관리 </h1> <br><br>
 
-<div style="height:100px; border: 1px solid gray; padding:30px 60px;">
-	이름<input type="text" id="iptSearch" value="" style="margin-left:30px;">
-	<select id="selPosition"  style="margin-left:8px; width: 150px;">
+    <div style="height:100px; border: 1px solid gray; padding:30px 60px;">
+        이름<input type="text" id="iptSearch" value="" style="margin-left:30px;">
+        <select id="selPosition" style="margin-left:8px; width: 150px;">
 
-	</select>
-	<input type="button" id="btnSearch" value="조회">
-</div>
-<div  style="padding: 20px 30px;float: right;">
-	<input type="button" id="btnDelete" style="" value="삭제">
-</div>
+        </select>
+        <input type="button" id="btnSearch" value="조회">
+    </div>
+    <div style="padding: 20px 30px;float: right;">
+        <input type="button" id="btnDelete" style="" value="삭제">
+    </div>
 
 </div>
 <div style="width:1200px;">
-	<table id="tbList" class = "ecoleTable ecoleTableFixed table table-hover dataTable">
-		<thead>
-			<tr>
-				<th><input type="checkbox" id="cbAllClick"></th>
-				<th>사번</th>
-				<th>이름</th>
-				<th>영문이름</th>
-				<th>직급</th>
-				<th>이메일주소</th>
-				<th>전화번호</th>
-				<th>생년월일</th>
-			</tr>
-		</thead>
-		<tbody>
+    <table id="tbList" class="ecoleTable ecoleTableFixed table table-hover dataTable">
+        <thead>
+        <tr>
+            <th><input type="checkbox" id="cbAllClick"></th>
+            <th>사번</th>
+            <th>이름</th>
+            <th>영문이름</th>
+            <th>직급</th>
+            <th>이메일주소</th>
+            <th>전화번호</th>
+            <th>생년월일</th>
+        </tr>
+        </thead>
+        <tbody>
 
-		</tbody>
-	</table>
+        </tbody>
+    </table>
 
 </div>
 
 <!-- 끝: 작성하기 -->
 <script type="text/javascript">
-	$.getScript(getContextPath() + "/resources/service/js/service/empList.js").done(function(script, textStatus) {
-		if (!!ecoletree.vc && ecoletree.vc.name === "empList") {
-			var inter = setInterval(function(){
-				ecoletree.promiseInit()
-						.then(function(){
-							clearInterval(inter);
-							ecoletree.vc.init(${initData});
-						}, function (){})
-			},300);
+    $.getScript(getContextPath() + "/resources/service/js/service/empList.js").done(function (script, textStatus) {
+        if (!!ecoletree.vc && ecoletree.vc.name === "empList") {
+            var inter = setInterval(function () {
+                ecoletree.promiseInit()
+                    .then(function () {
+                        clearInterval(inter);
+                        ecoletree.vc.init(${initData});
+                    }, function () {
+                    })
+            }, 300);
 
-		} else {
-			console.log("vc's name is not index : " + ecoletree.vc.name);
-		}
-	}).fail(function(jqxhr, settings, exception) {
-		console.log(arguments);
-	});
+        }
+        else {
+            console.log("vc's name is not index : " + ecoletree.vc.name);
+        }
+    }).fail(function (jqxhr, settings, exception) {
+        console.log(arguments);
+    });
 </script>
--- EcoleTreeMVC_ojt/src/main/webapp/resources/service/js/service/empList.js
+++ EcoleTreeMVC_ojt/src/main/webapp/resources/service/js/service/empList.js
@@ -5,117 +5,132 @@
  * @CreateDate : 2024. 01. 02.
  * @DESC : script sample
  ******************************************************************************/
-(function (et, ctrl) {
-    if (_.isObject(et) && et.name === ETCONST.PROJECT_NAME) {
-        if (!et.vc || et.vc.name !== "empList") {
-            et.vc = ctrl(et);
-        }
-
-    } else {
-        console.error("ecoletree OR ETCONST is not valid. please check that common.js file was imported.");
-    }
-}(this.ecoletree, function (et) {
-
-    "use strict";
-
-    var ctrl = {};
-
-    ctrl.name = "empList";
-    ctrl.path = "/sample";
-
-
-    // ============================== 화면 컨트롤 ==============================
-    /**
-     * init VIEW
-     */
-    ctrl.init = function (initData) {
-        var self = et.vc;
-        console.log("test");
-        ETService().setSuccessFunction(self.resultFunction).callService(self.path + "/code", {});
-
-        $("#btnSearch").click(self.btnSearchHandler);
-
-        et.setDataTableRowSelection("#tbList", self.rowSelectionHandler);
-    };
-
-    ctrl.rowSelectionHandler = function ($target, row, col, columnVisible) {
-
-        var self = et.vc;
-        if (col !== 0) {
-            var rowData = et.getRowData("#tbList", $target.closest("tr"));
-            console.log(rowData);
-
-            ETService().callView("/emp/update", rowData);
-            debugger;
-        }
-
-    }
-
-    ctrl.resultFunction = function (result) {
-        var self = et.vc;
-        var empList = null;
-        if (result.message === "success") {
-            empList = result.data;
-
-            var options = {
-                value: "code_cd",
-                text: "code_name"
+(
+    function (et, ctrl) {
+        if (_.isObject(et) && et.name === ETCONST.PROJECT_NAME) {
+            if (!et.vc || et.vc.name !== "empList") {
+                et.vc = ctrl(et);
             }
 
-            et.makeSelectOption(empList, options, "#selPosition", "전체");
+        }
+        else {
+            console.error("ecoletree OR ETCONST is not valid. please check that common.js file was imported.");
+        }
+    }(this.ecoletree, function (et) {
+
+        "use strict";
+
+        var ctrl = {};
+
+        ctrl.name = "empList";
+        ctrl.path = "/sample";
+
+
+        // ============================== 화면 컨트롤 ==============================
+        /**
+         * init VIEW
+         */
+        ctrl.init = function (initData) {
+            var self = et.vc;
+            ETService().setSuccessFunction(self.resultFunction).callService(self.path + "/code", {});
+
+            $("#btnSearch").click(self.btnSearchHandler);
+
+            // 체크박스 전체 선택
+            $("#cbAllClick").click(self.checkBoxClickHandler);
+
+            et.setDataTableRowSelection("#tbList", self.rowSelectionHandler);
+        };
+
+        ctrl.checkBoxClickHandler = function () {
+            var checked = $("#cbAllClick").prop("checked");
+            if (checked) {
+                $("input:checkbox").prop("checked", true);
+            } else {
+                $("input:checkbox").prop("checked", false);
+            }
         }
 
-    }
+        ctrl.rowSelectionHandler = function ($target, row, col, columnVisible) {
+            var self = et.vc;
+            if (col === 0 || col === undefined) {
 
-    // ============================== 동작 컨트롤 ==============================
+            }
+            else {
+                var rowData = et.getRowData("#tbList", $target.closest("tr"));
+                console.log('rowData', rowData);
+                debugger;
+                ETService().callView("/emp/update", rowData);
+            }
+        }
 
-    // ============================== 이벤트 리스너 ==============================
-    ctrl.btnSearchHandler = function () {
-        var self = et.vc;
-        var param = {};
+        ctrl.resultFunction = function (result) {
+            var self = et.vc;
+            var empList = null;
+            if (result.message === "success") {
+                empList = result.data;
 
-        param.emp_name = $("#iptSearch").val();
-        param.position = $("#selPosition").val();
-        // debugger;
-        self.createDateTables(param);
-
-    }
-    // ============================== DataTables 생성, 이벤트들 ==============================
-
-    ctrl.createDateTables = function (params) {
-        var self = et.vc;
-        const columns = [
-            {
-                data: "emp_num", render: function (data, type, row, meta) {
-                    return '<input type="checkbox">'
+                var options = {
+                    value: "code_cd",
+                    text : "code_name"
                 }
-            },
-            {data: "emp_name"},
-            {data: "emp_engname"},
-            {data: "position_name"},
-            {data: "email_1"},
-            {data: "phone_num"},
-            {data: "birthday"}
-        ];
 
-        const option = et.createDataTableSettings(
-            columns,
-            self.path + "/list",
-            params,
-            self.dataTableCallback
-        )
+                et.makeSelectOption(empList, options, "#selPosition", "전체");
+            }
 
-        option.paging = false;
+        }
 
-        $("#tbList").DataTable(option);
-    }
+        // ============================== 동작 컨트롤 ==============================
 
-    ctrl.dataTableCallback = function (settings) {
+        // ============================== 이벤트 리스너 ==============================
+        ctrl.btnSearchHandler = function () {
+            var self = et.vc;
+            var param = {};
 
-    }
+            param.emp_name = $("#iptSearch").val();
+            param.position = $("#selPosition").val();
+            // debugger;
+            self.createDateTables(param);
 
-    // ============================== Form 리스너 ==============================
+        }
+        // ============================== DataTables 생성, 이벤트들 ==============================
+
+        ctrl.createDateTables = function (params) {
+            var self = et.vc;
+            const columns = [
+                {
+                    data: "", render: function (data, type, row, meta) {
+                        return '<input type="checkbox" class="ckb">'
+                    }
+                },
+                {data: "emp_num"},
+                {data: "emp_name"},
+                {data: "emp_engname"},
+                {data: "position_name"},
+                {data: "email_1"},
+                {data: "phone_num"},
+                {data: "birthday"}
+            ];
+
+            const option = et.createDataTableSettings(
+                columns,
+                self.path + "/list",
+                params,
+                self.dataTableCallback
+            )
+
+            option.paging = false;
+
+            $("#tbList").DataTable(option);
+        }
+
+        ctrl.dataTableCallback = function (settings) {
+
+        }
+
+        // ============================== Form 리스너 ==============================
 
 
-    return ctrl;
-}));
+        return ctrl;
+    })
+);
