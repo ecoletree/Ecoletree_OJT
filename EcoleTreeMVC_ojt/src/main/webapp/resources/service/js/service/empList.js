863afcad0f019996f251ac51eacb190ee3eac403
kkh <ecoletree@gmail.com>
2024-01-04 14:57:54 +0900

    empList,empUpdate 수정

--- EcoleTreeMVC_ojt/src/main/webapp/resources/service/js/service/empList.js
+++ EcoleTreeMVC_ojt/src/main/webapp/resources/service/js/service/empList.js
@@ -1,9 +1,9 @@
 /*******************************************************************************
  * Copyright (c) 2017 Ecoletree. All Rights Reserved.
  * 
- * @Author : 
+ * @Author : kkh
  * @CreateDate : 2023. 12. 06.
- * @DESC : 
+ * @DESC : 사원관리
  ******************************************************************************/
 (function(et, ctrl) {
 	if (_.isObject(et) && et.name === ETCONST.PROJECT_NAME) {
@@ -15,88 +15,151 @@
 		console.error("ecoletree OR ETCONST is not valid. please check that common.js file was imported.");
 	}
 }(this.ecoletree, function(et) {
-	
 	"use strict";
 	
 	var ctrl = {};
 	
 	ctrl.name = "empList";
-	ctrl.path = "/sample";
-
+	ctrl.path = "/emp";
 	
 	// ============================== 화면 컨트롤 ==============================
 	/**
 	 * init VIEW
 	 */
-	/**
-	 */
 	ctrl.init = function(initData) {
 		var self = et.vc;
-		ETService().setSuccessFunction(self.codeSuccessResultFunciton).callService(self.path +"/code", {});
-		$("#btnSearch").click(self.btnSearchHandler);
 		
-		et.setDataTableRowSelection("#tbList", self.rowClickHandler);
+		new ETService().setSuccessFunction(self.getCodeListHandler).callService("/sample/code", {});
+
+		self.btnClickHandler();
+
+		$("#btnSearch").click(self.btnSearchClickHandler);
+		$("#cbAllClick").click(self.cbAllClickHandler);
+		$("#btnDelete").click(self.btnclickListHandler);
+//		$("#btnSearch").trigger("click");
+		
+		// 엔터키 이벤트 핸들러(검색 핸들러 function 넣으시면 됩니다)
+		et.setEnterKeyDownEvent("#iptSearch",self.btnSearchClickHandler);
+
+		et.setDataTableRowSelection("#tbList",self.tbListRowClickHandler);
 		
 		
 	};
 
-//콜백함수
-	ctrl.codeSuccessResultFunciton = function(result){
-		var self = et.vc;
-		var codeList = result.data;
-		//et 는 common.js에서 찾으면 됩니다.
-		et.makeSelectOption(codeList, {value:"code_cd", text:"code_name"}, "#selPosition", "전체");
-	}
 	// ============================== 동작 컨트롤 ==============================
 
-	// ============================== 이벤트 리스너 ==============================
-	ctrl.btnSearchHandler = function(){
+	/**
+	 * 코드 리스트 가져오기
+	 */
+	ctrl.getCodeListHandler = function(result){
 		var self = et.vc;
-		var param = {};
-		param.emp_name = $("#iptSearch").val();
-		param.position = $("#selPosition").val();
-		
-		console.log(param);
-		self.createDateTables(param);
+		var codeList = result.data;
+		et.makeSelectOption(codeList, {value:"code_cd",text:"code_name"}, "#selPosition", "전체");
 	}
 	
+	// ============================== 이벤트 리스너 ==============================
+	/**
+	 * 검색버튼 클릭 핸들러
+	 */	
+	ctrl.btnSearchClickHandler = function(){
+		var self = et.vc;
+		var params = {};
+		params.emp_name = $("#iptSearch").val();
+		params.position = $("#selPosition").val();
+		self.createDataTables(params);
+	}
 	
+	/**
+	 * 체크박스 전체 선택 핸들러
+	 */
+	ctrl.cbAllClickHandler = function(){//체크박스 전체 선택
+		if ($("#cbAllClick").is(':checked')) {
+			$("input[type=checkbox]").prop("checked",true);
+		}else {
+			$("input[type=checkbox]").prop("checked",false);
+		}
+	}
 	// ============================== DataTables 생성, 이벤트들 ==============================
-
-	ctrl.createDateTables =function(postData){
+	
+	/**
+	 * 테이블 생성
+	 */
+	ctrl.createDataTables = function(postData){
 		var self = et.vc;
 		var columns = [
-			{data:"emp_num",render:function(data,type,row,meta){
-				return '<input type="checkbox" >'
-			}},
-			{data:"emp_num"},
-			{data:"emp_name"},
-			{data:"emp_engname"},
-			{data:"position"}, //-> 과제 : 코드로 나오는거 이름으로 렌더해오기
-			{data:"position"},
-			{data:"position"},
-			{data:"position"},
-		];
-		var option = et.createDataTableSettings(columns, self.path+"/list", postData, self.dataTableCallback);
+			{data : "emp_cd", render:function(data,type,row,meta){
+				return '<input type="checkbox">';
+			}}
+			,{data : "emp_num"}
+			,{data : "emp_name"}
+			,{data : "emp_engname"}
+			,{data : "position",render:function(data,type,row){
+				return row.position_name;
+			}}
+			,{data : "email_1"}
+			,{data : "phone_num"}
+			,{data : "birthday"}
+		]
+		
+		var option = et.createDataTableSettings(columns,"/sample/list", postData, self.dataTableDrawCallback);
 		option.paging = false;
 		
 		$("#tbList").DataTable(option);
-		
-	}
-	ctrl.dataTableCallback = function(settings){
-		var self = et.vc;
 	}
 	
-	ctrl.rowClickHandler = function($target,row,col){
+	/**
+	 * 테이블 생성 후 콜백
+	 */
+	ctrl.dataTableCallback = function(settings){
 		var self = et.vc;
-		if(col === 0 || col === undefined){
-		}else{
-			var rowData = et.getRowData("#tbList", $target.closest("tr"));
-			
-			ETService().callView("/emp/update",rowData);
-			
+		console.log("data table list");
+	}
+	
+	/**
+	 * 테이블 로우 클릭 핸들러
+ 	 * et.getRowData = function(tableId, row)
+	 * rowClickAction($target, cell.row, cell.column, cell.columnVisible);
+	 */
+	ctrl.tbListRowClickHandler = function($target, row, col){
+		var self = et.vc;
+		if(column === 0 || column === undefined){}else{
+				var rowData = et.getRowData("#tbList",$target.closest("tr"));
+				new ETService().callView(self.path+"/update",rowData);
+		}
+		
+		
+	}
+	/**
+	 * 테이블 로우 선택 삭제 delete
+	 */
+	ctrl.btnclickListHandler = function() {
+		var self = et.vc;
+		
+		var paramArr = [];
+		$("#tbList tbody input:checked").each(function(index){
+			var chkData = et.getRowData("#tbList", $(this).parents("tr"));
+			paramArr.push(chkData);
+		});
+		var chkParam = {};
+		chkParam.paramKey = paramArr;
+		
+		if (chkParam.paramKey.length === 0) {
+			alert("삭제할 데이터를 체크해 주세요.");
+			return;
+		}else {
+			console.log(chkParam);
+//			new ETService().setSuccessFunction(self.userDeleteSuccessHandler).callService(self.path+"/delete", chkParam);
 		}
 	}
+	/**
+	 * 테이블 로우 선택 삭제 delete 콜백
+	 */
+	ctrl.userDeleteSuccessHandler = function (result) {
+		var self = et.vc;
+		alert("삭제 완료");
+		location.reload();
+	}
+	
 	// ============================== Form 리스너 ==============================
 
 	
