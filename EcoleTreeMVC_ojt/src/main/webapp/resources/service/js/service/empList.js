5b1564633c2f2239833f63a3d658d5f46173e3ab
Jeongjin Kim <valorjj@gmail.com>
2024-01-07 23:12:44 +0900

    2023-01-07 update, create js 수정

--- EcoleTreeMVC_ojt/src/main/webapp/resources/service/js/service/empList.js
+++ EcoleTreeMVC_ojt/src/main/webapp/resources/service/js/service/empList.js
@@ -6,226 +6,229 @@
  * @DESC : script sample
  ******************************************************************************/
 (
-  function (et, ctrl) {
-    if (_.isObject(et) && et.name === ETCONST.PROJECT_NAME) {
-      if (!et.vc || et.vc.name !== 'empList') {
-        et.vc = ctrl(et)
-      }
+    function (et, ctrl) {
+        if (_.isObject(et) && et.name === ETCONST.PROJECT_NAME) {
+            if (!et.vc || et.vc.name !== 'empList') {
+                et.vc = ctrl(et)
+            }
 
-    }
-    else {
-      console.error('ecoletree OR ETCONST is not valid. please check that common.js file was imported.')
-    }
-  }(this.ecoletree, function (et) {
+        }
+        else {
+            console.error('ecoletree OR ETCONST is not valid. please check that common.js file was imported.')
+        }
+    }(this.ecoletree, function (et) {
 
-    'use strict'
+        'use strict'
 
-    var ctrl = {}
+        var ctrl = {}
 
-    ctrl.name = 'empList'
-    ctrl.path = '/sample'
+        ctrl.name = 'empList'
+        ctrl.path = '/sample'
 
-    // ============================== 화면 컨트롤 ==============================
-    /**
-     * init VIEW
-     */
-    ctrl.init = function (initData) {
-      var self = et.vc
+        // ============================== 화면 컨트롤 ==============================
+        /**
+         * init VIEW
+         */
+        ctrl.init = function (initData) {
+            var self = et.vc
 
-      ETService()
-        .setSuccessFunction(self.resultFunction)
-        .setErrorFunction(self.errorFunction)
-        .callService(self.path + '/code', {})
+            ETService()
+                .setSuccessFunction(self.resultFunction)
+                // .setErrorFunction(self.errorFunction)
+                .callService(self.path + '/code', {})
 
-      self.eventHandlers()
+            self.eventHandlers()
 
-      et.setDataTableRowSelection('#tbList', self.rowSelectionHandler)
-    }
-
-    /**
-     * 테이블에서 '행'을 선택했을 때 발생하는 이벤트입니다.
-     *
-     * @param {object} $target
-     * @param {string} row
-     * @param {string} col
-     * @param {boolean} columnVisible
-     * */
-    ctrl.rowSelectionHandler = function ($target, row, col, columnVisible) {
-      var self = et.vc
-
-      if (col !== 0 && col !== undefined) {
-        let rowData = et.getRowData('#tbList', $target.closest('tr'))
-        ETService().callView('/emp/update', rowData)
-      }
-    }
-
-    /**
-     * 서버에서 응답받은 결과를 처리하는 함수입니다.
-     *
-     * @param {object} result 서버 응답 결과
-     * */
-    ctrl.resultFunction = function (result) {
-      var self = et.vc
-      let empList = null
-      if (result.message === 'success') {
-        empList = result.data
-
-        let options = {
-          value: 'code_cd',
-          text : 'code_name'
+            et.setDataTableRowSelection('#tbList', self.rowSelectionHandler)
         }
 
-        et.makeSelectOption(empList, options, '#selPosition', '전체')
-      }
-      // when error,
-      else {
-        // TBD
-      }
-    }
+        /**
+         * 테이블에서 '행'을 선택했을 때 발생하는 이벤트입니다.
+         *
+         * @param {object} $target 선택한 객체
+         * @param {number} row 선택한 행
+         * @param {number} col 선택한 열
+         * @param {boolean} columnVisible
+         * */
+        ctrl.rowSelectionHandler = function ($target, row, col, columnVisible) {
+            var self = et.vc
 
-    /**
-     * 클릭 이벤트를 담당하는 이벤트 핸들러 모음
-     * */
-    ctrl.eventHandlers = function () {
-      var self = et.vc
+            if (col !== 0 && col !== undefined) {
+                let rowData = et.getRowData('#tbList', $target.closest('tr'))
+                ETService().callView('/emp/update', rowData)
+            }
+        }
 
-      $('#btnSearch').click(self.btnSearchHandler)
-      $('#cbAllClick').click(self.checkBoxClickHandler)
-      $('#btnDelete').click(self.deleteBtnHandler)
-      $('#tbList tbody input:checkbox').click(self.singleCheckBoxHandler)
-    }
+        /**
+         * 서버에서 응답받은 결과를 처리하는 함수입니다.
+         *
+         * @param {object} response 서버 응답 결과
+         * */
+        ctrl.resultFunction = function (response) {
+            var self = et.vc
 
-    // ============================== 동작 컨트롤 ==============================
+            let empList = null
+            if (response.message === 'success') {
+                empList = response.data
 
-    /**
-     * 헤더에 위치한 체크박스를 누르면 테이블에 존재하는 모든 행을 선택합니다. 혹은 선택을 해제합니다.
-     *
-     * */
-    ctrl.checkBoxClickHandler = function () {
-      var self = et.vc
-      let checked = $('#cbAllClick').prop('checked')
+                et.makeSelectOption(empList, {
+                    value: 'code_cd',
+                    text : 'code_name'
+                }, '#selPosition', '전체')
+            }
+            // when error,
+            else {
+                // TBD
+            }
+        }
 
-      if (checked) {
-        $('input:checkbox').prop('checked', true)
-      }
-      else {
-        $('input:checkbox').prop('checked', false)
-      }
+        /**
+         * 클릭 이벤트를 담당하는 이벤트 핸들러 모음
+         * */
+        ctrl.eventHandlers = function () {
+            var self = et.vc
 
-    }
+            $('#btnSearch').click(self.btnSearchHandler)
+            $('#cbAllClick').click(self.checkBoxClickHandler)
+            $('#btnDelete').click(self.deleteBtnHandler)
+            $('#tbList tbody input:checkbox').click(self.singleCheckBoxHandler)
+        }
 
-    // ============================== 이벤트 리스너 ==============================
+        // ============================== 동작 컨트롤 ==============================
 
-    /**
-     * 검색 버튼을 눌렀을 때 서버에 요청을 보내는 함수에 검색값을 전달합니다.
-     *
-     * */
-    ctrl.btnSearchHandler = function () {
-      var self = et.vc
-      let param = {}
+        /**
+         * 헤더에 위치한 체크박스를 누르면 테이블에 존재하는 모든 행을 선택합니다. 혹은 선택을 해제합니다.
+         *
+         * */
+        ctrl.checkBoxClickHandler = function () {
+            var self = et.vc
 
-      param.emp_name = $('#iptSearch').val()
-      param.position = $('#selPosition').val()
+            let checked = $('#cbAllClick').prop('checked')
 
-      self.createDateTables(param)
-    }
+            if (checked) {
+                $('input:checkbox').prop('checked', true)
+            }
+            else {
+                $('input:checkbox').prop('checked', false)
+            }
 
-    /**
-     * 체크박스 하나를 클릭했을 때 이벤트를 처리합니다. 모든 항목을 체크하면 전체 목록를 선택하는 체크박스를 체크,
-     * 모든 항목이 체크되어 있는 상황에서 개별 체크박스를 선택해제 시키면 전체 목록를 선택하는 체크박스의 체크를 해제합니다.
-     *
-     * */
-    ctrl.singleCheckBoxHandler = function () {
-      let checks = $('input:checkbox:not(#cbAllClick)')
-      let checked = $('input:checkbox:not(#cbAllClick):checked')
+        }
 
-      if (checks.length === checked.length) {
-        $('#cbAllClick').prop('checked', true)
-      }
-      else {
-        $('#cbAllClick').prop('checked', false)
-      }
-    }
+        // ============================== 이벤트 리스너 ==============================
 
-    /**
-     * 삭제 버튼을 눌렀을 때 선택한 행 데이터를 삭제합니다.
-     *
-     * */
-    ctrl.deleteBtnHandler = function () {
-      var self = et.vc
-      let del = {}
-      $('#tbList tbody input:checkbox:checked').each(function (index, element) {
-        del[index] = (
-          et.getRowData('#tbList', this.closest('tr'))
-        )
-      })
+        /**
+         * 검색 버튼을 눌렀을 때 서버에 요청을 보내는 함수에 검색값을 전달합니다.
+         *
+         * */
+        ctrl.btnSearchHandler = function () {
+            var self = et.vc
 
-      // console.log(del)
-      // {0 : { key : value }, 1 : { key : value },...}
+            let param = {}
 
-      // 선택한 데이터가 존재하지 않는 경우
-      if (del.length === 0) {
-        alert('삭제할 데이터를 선택해주세요.')
-      }
-      else {
-        // 아직 컨트롤러가 없는 관계로 주석처리
-        // et.callService(self.path + "/delete", del);
-      }
+            param.emp_name = $('#iptSearch').val()
+            param.position = $('#selPosition').val()
 
-    }
-    // ============================== DataTables 생성, 이벤트들 ==============================
+            self.createDateTables(param)
+        }
 
-    /**
-     * 서버에 데이터를 요청해서 받아온 결과를 테이블에 출력합니다.
-     *
-     * @param {object} searchParams search parameters
-     * */
-    ctrl.createDateTables = function (searchParams) {
-      var self = et.vc
-      let columns = [
-        {
-          // render is from library
-          data: '', render: function (data, type, row, meta) {
-            return '<input type="checkbox">'
-          }
-        },
-        { data: 'emp_num' },
-        { data: 'emp_name' },
-        { data: 'emp_engname' },
-        {
-          data: 'position', render: function (data, type, row, meta) {
-            return row.position_name
-          }
-        },
-        { data: 'email_1' },
-        { data: 'phone_num' },
-        { data: 'birthday' }
-      ]
+        /**
+         * 체크박스 하나를 클릭했을 때 이벤트를 처리합니다. 모든 항목을 체크하면 전체 목록를 선택하는 체크박스를 체크,
+         * 모든 항목이 체크되어 있는 상황에서 개별 체크박스를 선택해제 시키면 전체 목록를 선택하는 체크박스의 체크를 해제합니다.
+         *
+         * */
+        ctrl.singleCheckBoxHandler = function () {
+            let checks = $('input:checkbox:not(#cbAllClick)')
+            let checked = $('input:checkbox:not(#cbAllClick):checked')
 
-      let option = et.createDataTableSettings(
-        columns,
-        self.path + '/list',
-        searchParams,
-        self.dataTableCallback
-      )
+            if (checks.length === checked.length) {
+                $('#cbAllClick').prop('checked', true)
+            }
+            else {
+                $('#cbAllClick').prop('checked', false)
+            }
+        }
 
-      // temporary disable paging option
-      option.paging = false
+        /**
+         * 삭제 버튼을 눌렀을 때 선택한 행 데이터를 삭제합니다.
+         *
+         * */
+        ctrl.deleteBtnHandler = function () {
+            var self = et.vc
 
-      $('#tbList').DataTable(option)
-    }
+            let del = {}
+            $('#tbList tbody input:checkbox:checked').each(function (index, element) {
+                del[index] = (
+                    et.getRowData('#tbList', this.closest('tr'))
+                )
+            })
 
-    /**
-     * !TBD, 동적 테이블의 콜백함수를 설정합니다.
-     *
-     * @param {object} settings         * */
-    ctrl.dataTableCallback = function (settings) {
-      var self = et.vc
+            // console.log(del)
+            // {0 : { key : value }, 1 : { key : value },...}
 
-    }
+            // 선택한 데이터가 존재하지 않는 경우
+            if (del.length === 0) {
+                alert('삭제할 데이터를 선택해주세요.')
+            }
+            else {
+                // 아직 컨트롤러가 없는 관계로 주석처리
+                // et.callService(self.path + "/delete", del);
+            }
 
-    // ============================== Form 리스너 ==============================
+        }
+        // ============================== DataTables 생성, 이벤트들 ==============================
 
-    return ctrl
-  })
+        /**
+         * 서버에 데이터를 요청해서 받아온 결과를 테이블에 출력합니다.
+         *
+         * @param {object} searchParams search parameters
+         * */
+        ctrl.createDateTables = function (searchParams) {
+            var self = et.vc
+
+            let columns = [
+                {
+                    // render is from library
+                    data: '', render: function (data, type, row, meta) {
+                        return '<input type="checkbox">'
+                    }
+                },
+                {data: 'emp_num'},
+                {data: 'emp_name'},
+                {data: 'emp_engname'},
+                {
+                    data: 'position', render: function (data, type, row, meta) {
+                        return row.position_name
+                    }
+                },
+                {data: 'email_1'},
+                {data: 'phone_num'},
+                {data: 'birthday'}
+            ]
+
+            let option = et.createDataTableSettings(
+                columns,
+                self.path + '/list',
+                searchParams,
+                self.dataTableCallback
+            )
+
+            // temporary disable paging option
+            option.paging = false
+
+            $('#tbList').DataTable(option)
+        }
+
+        /**
+         * !TBD, 동적 테이블의 콜백함수를 설정합니다.
+         *
+         * @param {object} settings         * */
+        ctrl.dataTableCallback = function (settings) {
+            var self = et.vc
+
+        }
+
+        // ============================== Form 리스너 ==============================
+
+        return ctrl
+    })
 )
--- EcoleTreeMVC_ojt/src/main/webapp/resources/service/js/service/updateEmp.js
+++ EcoleTreeMVC_ojt/src/main/webapp/resources/service/js/service/updateEmp.js
@@ -6,209 +6,218 @@
  * @DESC : script sample
  ******************************************************************************/
 (
-  function (et, ctrl) {
-    if (_.isObject(et) && et.name === ETCONST.PROJECT_NAME) {
-      if (!et.vc || et.vc.name !== 'updateEmp') {
-        et.vc = ctrl(et)
-      }
+    function (et, ctrl) {
+        if (_.isObject(et) && et.name === ETCONST.PROJECT_NAME) {
+            if (!et.vc || et.vc.name !== 'updateEmp') {
+                et.vc = ctrl(et)
+            }
 
-    }
-    else {
-      console.error('ecoletree OR ETCONST is not valid. please check that common.js file was imported.')
-    }
-  }(this.ecoletree, function (et) {
-
-    'use strict'
-
-    var ctrl = {}
-
-    ctrl.name = 'updateEmp'
-    ctrl.path = '/sample'
-
-    // ============================== 화면 컨트롤 ==============================
-    /**
-     * init VIEW
-     */
-    ctrl.init = function (initData) {
-      var self = et.vc
-
-      ctrl.initData = initData
-
-      self.setSelect()
-      self.setInputView('#editForm', initData.rowData)
-      self.validation()
-
-      // edit button click event
-      $('#btnEdit').click(self.btnEditClickHandler)
-    }
-
-    /**
-     * select 박스에 들어가는 데이터를 화면에 출력합니다.
-     * */
-    ctrl.setSelect = function () {
-      var self = et.vc
-
-      let options = {
-        value: 'code_cd',
-        text : 'code_name'
-      }
-
-      et.makeSelectOption(
-        self.initData.codeList,
-        options,
-        '#selPosition',
-        '전체'
-      )
-    }
-
-    /**
-     * empList.jsp 테이블에서 선택한 행 데이터를 화면에 보여줍니다. (select 에 들어가는 데이터 제외)
-     *
-     * @param {string} formId form 아이디
-     * @param {object} rowData 이전화면에서 선택한 row 데이터
-     * */
-    ctrl.setInputView = function (formId, rowData) {
-      var self = et.vc
-
-      let triggerElement = []
-
-      $(formId).find('input, select').each(function (index, element) {
-        const name = $(element).prop('name')
-        if ($(element).prop('localName') === 'select') {
-          if (rowData[name] === undefined) {
-            $(element).children('option:eq(0)').prop('selected', true)
-          }
-          else {
-            $(element).val(rowData[name])
-            triggerElement.push(element)
-          }
         }
-        else if ($(element).prop('localName') === 'input') {
-          $(element).val(rowData[name] === undefined ? '' : rowData[name])
+        else {
+            console.error('ecoletree OR ETCONST is not valid. please check that common.js file was imported.')
         }
-      })
+    }(this.ecoletree, function (et) {
 
-      $.each(triggerElement, function (index, element) {
-        if ($(element).prop('localName') === 'select') {
-          $(element).trigger('change')
+        'use strict'
+
+        var ctrl = {}
+
+        ctrl.name = 'updateEmp'
+        ctrl.path = '/sample'
+
+
+        // ============================== 화면 컨트롤 ==============================
+        /**
+         * init VIEW
+         */
+        ctrl.init = function (initData) {
+            var self = et.vc
+
+            ctrl.initData = initData
+            ctrl.regExp = {};   // 정규식 패턴을 담고 있는 객체
+
+            self.setRegExp()    // 정규식 등록
+            self.setSelect()    // select tag 데이터 입력
+            self.setInputView('#editForm', initData.rowData)    // form 데이터 집어넣기
+            self.validation()   // 유효성 검사
+
+            $('#btnEdit').click(self.btnEditClickHandler)
         }
-      })
-    }
 
-    // ============================== 동작 컨트롤 ==============================
+        /**
+         * 유효성 검사를 위한 정규식을 담고 있는 메서드
+         * */
+        ctrl.setRegExp = function () {
+            var self = et.vc;
 
-    // ============================== 이벤트 리스너 ==============================
+            // 핸드폰 번호 확인 정규식
+            self.regExp['phoneReg'] = /^\d{3}-\d{4}-\d{4}$/g
+            // 이메일 확인 정규식
+            self.regExp['emailReg'] = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i
+            // 특수문자 확인 정규식
+            self.regExp['irregularCharReg'] = /[!?@#$%^&*():;+-=~{}<>_[\]|\\"',./`₩]/g
+            // 빈칸 확인 정규식
+            self.regExp['whiteSpaceReg'] = /\s/g
+        }
 
-    /**
-     * 수정 버튼을 클릭했을 시, form 을 제출하는 함수를 호출합니다.
-     * */
-    ctrl.btnEditClickHandler = function () {
-      var self = et.vc
+        /**
+         * select 박스에 들어가는 데이터를 화면에 출력합니다.
+         * */
+        ctrl.setSelect = function () {
+            var self = et.vc
 
-      $('#editForm').submit()
-    }
-    // ============================== DataTables 생성, 이벤트들 ==============================
+            et.makeSelectOption(
+                self.initData.codeList,
+                {
+                    value: 'code_cd',
+                    text : 'code_name'
+                },
+                '#selPosition',
+                '전체'
+            )
+        }
 
-    // ============================== Form 리스너 ==============================
+        /**
+         * empList.jsp 테이블에서 선택한 행 데이터를 화면에 보여줍니다. (select 에 들어가는 데이터 제외)
+         *
+         * @param {string} formId form 아이디
+         * @param {object} rowData 이전화면에서 선택한 row 데이터
+         * */
+        ctrl.setInputView = function (formId, rowData) {
+            var self = et.vc
 
-    /**
-     * form 데이터의 유효성을 검사합니다. 필수 조건 충족 여부, 길이 제한 등을 검사합니다.
-     *
-     * */
-    ctrl.validation = function () {
-      var self = et.vc
+            let triggerElement = []
 
-      let editValidation
-        = new ETValidate('#editForm', self.path + '/update')
-        .setSubmitHandler(self.editSubmitHandler)
-        .setShowErrors(et.setErrorFunction())
+            $(formId).find('input, select').each(function (index, element) {
+                const name = $(element).prop('name')
+                if ($(element).prop('localName') === 'select') {
+                    if (rowData[name] === undefined) {
+                        $(element).children('option:eq(0)').prop('selected', true)
+                    }
+                    else {
+                        $(element).val(rowData[name])
+                        triggerElement.push(element)
+                    }
+                }
+                else if ($(element).prop('localName') === 'input') {
+                    $(element).val(rowData[name] === undefined ? '' : rowData[name])
+                }
+            })
 
-      // 핸드폰 번호 확인 정규식
-      const phoneReg = /^\d{3}-\d{4}-\d{4}$/ | /^\d{11}$/
-      // 이메일 확인 정규식
-      const emailReg = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i
-      // 특수문자 확인 정규식
-      const irregularCharReg = /[!?@#$%^&*():;+-=~{}<>_\[\]|\\"',.\/`₩]/g
-      // 빈칸 확인 정규식 (ETValidate 에 trim
-      const whiteSpaceReg = /\s/
+            $.each(triggerElement, function (index, element) {
+                if ($(element).prop('localName') === 'select') {
+                    $(element).trigger('change')
+                }
+            })
+        }
 
-      // 커스텀 룰 - 핸드폰 형식 체크
-      ETValidate.addMethod('phoneReg', function (value, element, param) {
-        return phoneReg.test(value.toString().trim()) || !whiteSpaceReg.test(value.toString())
-      })
-      // 커스텀 룰 - 이메일 형식 체크
-      ETValidate.addMethod('emailReg', function (value, element, params) {
-        return !emailReg.test(value.toString())
-      })
-      // 커스텀 규칙 - 특수문자 체크
-      ETValidate.addMethod('irregularCharReg', function (value, element, params) {
-        return irregularCharReg.test(value.toString())
-      })
+        // ============================== 동작 컨트롤 ==============================
 
-      // (1) emp_name
-      editValidation.validateRules(
-        'emp_name',
-        'irregularCharReg',
-        '이름은 필수값 입니다.'
-      )
-      // (2) department
-      editValidation.validateRules(
-        'department',
-        editValidation.REQUIRED,
-        '부서는 필수값 입니다.'
-      )
-      // (3) position
-      editValidation.validateRules(
-        'position',
-        editValidation.REQUIRED,
-        '직위는 필수값 입니다.'
-      )
+        // ============================== 이벤트 리스너 ==============================
 
-      // (4) email
-      editValidation.validateRules(
-        'email_1',
-        'emailReg',
-        '메일은 필수값 입니다.'
-      )
+        /**
+         * 수정 버튼을 클릭했을 시, form 을 제출하는 함수를 호출합니다.
+         * */
+        ctrl.btnEditClickHandler = function () {
+            var self = et.vc
 
-      // (5) phone_num
-      editValidation.validateRules(
-        'phone_num',
-        'phoneReg',
-        '유효한 전화번호가 아닙니다.'
-      )
+            $('#editForm').submit()
+        }
+        // ============================== DataTables 생성, 이벤트들 ==============================
 
-      editValidation.apply()
-    }
+        // ============================== Form 리스너 ==============================
 
-    /**
-     * 유효성 검사를 통과한 form 데이터를 submit 합니다.
-     *
-     * @param {object} form HTMLFormElement
-     * */
-    ctrl.editSubmitHandler = function () {
-      var self = et.vc
-      let form = $('#editForm')
-      // form 데이터 직렬화
-      let formData = ETValidate.convertFormToObject(form, true, true)
-      console.log(formData)
+        /**
+         * form 데이터의 유효성을 검사합니다. 필수 조건 충족 여부, 길이 제한 등을 검사합니다.
+         *
+         * */
+        ctrl.validation = function () {
+            var self = et.vc
 
-      // ! 현재 /sample/delete 생성 대기 중
-      // new ETService()
-      //     .setSuccessFunction(self.editSuccessHandler)
-      //     .callService(self.path + "/delete", formData);
-    }
+            let editValidation
+                = new ETValidate('#editForm', self.path + '/update')
+                .setSubmitHandler(self.editSubmitHandler)
+                .setShowErrors(et.setErrorFunction())
 
-    /**
-     * form 제출 후, 서버에서 전송한 응답을 처리합니다.
-     *
-     * @param {object} result 서버 응답
-     * */
-    ctrl.editSuccessHandler = function (result) {
-      var self = et.vc
 
-    }
+            // 커스텀 룰 - 핸드폰 형식 체크
+            ETValidate.addMethod('phoneReg', function (value, element, param) {
+                return self.regExp.phoneReg.test(value.toString().trim())
+            })
+            // 커스텀 룰 - 이메일 형식 체크
+            ETValidate.addMethod('emailReg', function (value, element, params) {
+                return self.regExp.emailReg.test(value.toString().trim())
+            })
+            // 커스텀 규칙 - 특수문자 체크
+            ETValidate.addMethod('irregularCharReg', function (value, element, params) {
+                return self.regExp.irregularCharReg.test(value.toString().trim())
+            })
 
-    return ctrl
-  })
+            // (1) emp_name
+            editValidation.validateRules(
+                'emp_name',
+                editValidation.REQUIRED,
+                '이름은 필수값 입니다.'
+            )
+            // (2) department
+            editValidation.validateRules(
+                'department',
+                editValidation.REQUIRED,
+                '부서는 필수값 입니다.'
+            )
+            // (3) position
+            editValidation.validateRules(
+                'position',
+                editValidation.REQUIRED,
+                '직위는 필수값 입니다.'
+            )
+
+            // (4) email
+            editValidation.validateRules(
+                'email_1',
+                'emailReg',
+                '메일은 필수값 입니다.'
+            )
+
+            // (5) phone_num
+            editValidation.validateRules(
+                'phone_num',
+                'phoneReg',
+                '유효한 전화번호가 아닙니다.'
+            )
+
+            editValidation.apply()
+        }
+
+        /**
+         * 유효성 검사를 통과한 form 데이터를 submit 합니다.
+         *
+         * @param {object} form HTMLFormElement
+         * */
+        ctrl.editSubmitHandler = function () {
+            var self = et.vc
+
+            let form = $('#editForm')
+            // form 데이터 직렬화
+            let formData = ETValidate.convertFormToObject(form, true, true)
+
+            console.log("[/sample/update] formData 확인", formData)
+
+            // new ETService()
+            //     .setSuccessFunction(self.editSuccessHandler)
+            //     .callService(self.path + "/update", formData);
+        }
+
+        /**
+         * form 제출 후, 서버에서 전송한 응답을 처리합니다.
+         *
+         * @param {object} result 서버 응답
+         * */
+        ctrl.editSuccessHandler = function (result) {
+            var self = et.vc
+
+        }
+
+        return ctrl
+    })
 )
