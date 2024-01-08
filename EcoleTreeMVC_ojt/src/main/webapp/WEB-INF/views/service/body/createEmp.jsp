aa346ae13604a0174097e597115d45ccc126dada
Jeongjin Kim <valorjj@gmail.com>
2024-01-08 11:18:58 +0900

    2024-01-08 정규식 추가

--- EcoleTreeMVC_ojt/src/main/webapp/WEB-INF/views/service/body/createEmp.jsp
+++ EcoleTreeMVC_ojt/src/main/webapp/WEB-INF/views/service/body/createEmp.jsp
@@ -1,88 +1,110 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <div style="width:1200px;">
-	<h1> 사원추가 - jQuery form validation</h1> <br><br>
+    <h1> 사원추가 - jQuery form validation</h1> <br><br>
 
-<div>
-<form id="addForm" method="post" autocomplete="off">
-	<fieldset>
-		<div class="formWrap">
-			<label class="formLabel">사번<span class="require" style="color:red;">*</span></label>
-			<div class="formInput">
-				<input type="text" name="emp_num" >
-			</div>
-		</div>
-		<div class="formWrap">
-			<label class="formLabel">이름<span class="require" style="color:red;">*</span></label>
-			<div class="formInput">
-				<input type="text" name="emp_name" >
-			</div>
-		</div>
-		<div class="formWrap">
-			<label class="formLabel">영문 이름</label>
-			<div class="formInput">
-				<input type="text" name="emp_engname" >
-			</div>
-		</div>
-		<div class="formWrap">
-			<label class="formLabel">부서<span class="require"style="color:red;">*</span></label>
-			<div class="formInput">
-				<input type="text" name="department">
-			</div>
-		</div>
-		<div class="formWrap">
-			<label class="formLabel">직급<span class="require"style="color:red;">*</span></label>
-			<div class="formInput">
-			<select id="selPosition"name="position">
-			</select>
-			</div>
-		</div>
-		<div class="formWrap">
-			<label class="formLabel">메일주소1<span class="require"style="color:red;">*</span></label>
-			<div class="formInput">
-				<input type="text" name="email_1">
-			</div>
-		</div>
-		<div class="formWrap">
-			<label class="formLabel">메일주소2</label>
-			<div class="formInput">
-				<input type="text" name="email_2">
-			</div>
-		</div>
-		<div class="formWrap">
-			<label class="formLabel">전화번호<span class="require"style="color:red;">*</span></label>
-			<div class="formInput">
-				<input type="text" name="phone_num">
-			</div>
-		</div>
-		<div class="formWrap">
-			<label class="formLabel">생년월일</label>
-			<div >
-				<input type="text" name="birthday">
-			</div>
-		</div>
-		<div class="formWrap">
-			<label class="formLabel">주소</label>
-			<div class="formInput">
-				<input type="text" name="address">
-			</div>
-		</div>
-		<br><br>
-		<h5>긴급 연락망</h5>
-		
-		<div class="formWrap"style="margin-top : 7px ;">
-			<label class="formLabel">이름(관계)</label>
-			<div class="formInput">
-				<input type="text" name="emc_contact_point">
-			</div>
-			<label class="formLabel">전화번호</label>
-			<div class="formInput">
-				<input type="text" name="emc_phone_num">
-			</div>
-		</div>
-	</fieldset>
-</form>
+    <div>
+        <form id="addForm" method="post" autocomplete="off">
+            <fieldset>
+                <div class="formWrap">
+                    <label class="formLabel">사번<span class="require" style="color:red;">*</span></label>
+                    <div class="formInput">
+                        <input type="text" name="emp_num">
+                    </div>
+                </div>
+                <div class="formWrap">
+                    <label class="formLabel">이름<span class="require" style="color:red;">*</span></label>
+                    <div class="formInput">
+                        <input type="text" name="emp_name">
+                    </div>
+                </div>
+                <div class="formWrap">
+                    <label class="formLabel">영문 이름</label>
+                    <div class="formInput">
+                        <input type="text" name="emp_engname">
+                    </div>
+                </div>
+                <div class="formWrap">
+                    <label class="formLabel">부서<span class="require" style="color:red;">*</span></label>
+                    <div class="formInput">
+                        <input type="text" name="department">
+                    </div>
+                </div>
+                <div class="formWrap">
+                    <label class="formLabel">직급<span class="require" style="color:red;">*</span></label>
+                    <div class="formInput">
+                        <select id="selPosition" name="position">
+                        </select>
+                    </div>
+                </div>
+                <div class="formWrap">
+                    <label class="formLabel">메일주소1<span class="require" style="color:red;">*</span></label>
+                    <div class="formInput">
+                        <input type="text" name="email_1">
+                    </div>
+                </div>
+                <div class="formWrap">
+                    <label class="formLabel">메일주소2</label>
+                    <div class="formInput">
+                        <input type="text" name="email_2">
+                    </div>
+                </div>
+                <div class="formWrap">
+                    <label class="formLabel">전화번호<span class="require" style="color:red;">*</span></label>
+                    <div class="formInput">
+                        <input type="text" name="phone_num">
+                    </div>
+                </div>
+                <div class="formWrap">
+                    <label class="formLabel">생년월일</label>
+                    <div>
+                        <input type="text" name="birthday">
+                    </div>
+                </div>
+                <div class="formWrap">
+                    <label class="formLabel">주소</label>
+                    <div class="formInput">
+                        <input type="text" name="address">
+                    </div>
+                </div>
+                <br><br>
+                <h5>긴급 연락망</h5>
+
+                <div class="formWrap" style="margin-top : 7px ;">
+                    <label class="formLabel">이름(관계)</label>
+                    <div class="formInput">
+                        <input type="text" name="emc_contact_point">
+                    </div>
+                    <label class="formLabel">전화번호</label>
+                    <div class="formInput">
+                        <input type="text" name="emc_phone_num">
+                    </div>
+                </div>
+            </fieldset>
+        </form>
+    </div>
+    <div>
+        <input id="btnAdd" type="button" value="추가">
+    </div>
 </div>
-<div >
-<input id="btnAdd" type="button" value ="추가">
-</div>
-</div>
+
+<!-- 끝: 작성하기 -->
+<script type="text/javascript">
+	$.getScript(getContextPath() + "/resources/service/js/service/createEmp.js").done(function (script, textStatus) {
+		if (!!ecoletree.vc && ecoletree.vc.name === "createEmp") {
+			var inter = setInterval(function () {
+				ecoletree.promiseInit()
+				.then(function () {
+					clearInterval(inter);
+					ecoletree.vc.init(${initData});
+				}, function () {
+				})
+			}, 300);
+
+		}
+		else {
+			console.log("vc's name is not index : " + ecoletree.vc.name);
+		}
+	}).fail(function (jqxhr, settings, exception) {
+		console.log(arguments);
+	});
+</script>
--- EcoleTreeMVC_ojt/src/main/webapp/resources/service/js/service/updateEmp.js
+++ EcoleTreeMVC_ojt/src/main/webapp/resources/service/js/service/updateEmp.js
@@ -6,218 +6,242 @@
  * @DESC : script sample
  ******************************************************************************/
 (
-    function (et, ctrl) {
-        if (_.isObject(et) && et.name === ETCONST.PROJECT_NAME) {
-            if (!et.vc || et.vc.name !== 'updateEmp') {
-                et.vc = ctrl(et)
-            }
-
-        }
-        else {
-            console.error('ecoletree OR ETCONST is not valid. please check that common.js file was imported.')
-        }
-    }(this.ecoletree, function (et) {
-
-        'use strict'
-
-        var ctrl = {}
-
-        ctrl.name = 'updateEmp'
-        ctrl.path = '/sample'
-
-
-        // ============================== 화면 컨트롤 ==============================
-        /**
-         * init VIEW
-         */
-        ctrl.init = function (initData) {
-            var self = et.vc
-
-            ctrl.initData = initData
-            ctrl.regExp = {};   // 정규식 패턴을 담고 있는 객체
-
-            self.setRegExp()    // 정규식 등록
-            self.setSelect()    // select tag 데이터 입력
-            self.setInputView('#editForm', initData.rowData)    // form 데이터 집어넣기
-            self.validation()   // 유효성 검사
-
-            $('#btnEdit').click(self.btnEditClickHandler)
-        }
-
-        /**
-         * 유효성 검사를 위한 정규식을 담고 있는 메서드
-         * */
-        ctrl.setRegExp = function () {
-            var self = et.vc;
-
-            // 핸드폰 번호 확인 정규식
-            self.regExp['phoneReg'] = /^\d{3}-\d{4}-\d{4}$/g
-            // 이메일 확인 정규식
-            self.regExp['emailReg'] = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i
-            // 특수문자 확인 정규식
-            self.regExp['irregularCharReg'] = /[!?@#$%^&*():;+-=~{}<>_[\]|\\"',./`₩]/g
-            // 빈칸 확인 정규식
-            self.regExp['whiteSpaceReg'] = /\s/g
-        }
-
-        /**
-         * select 박스에 들어가는 데이터를 화면에 출력합니다.
-         * */
-        ctrl.setSelect = function () {
-            var self = et.vc
-
-            et.makeSelectOption(
-                self.initData.codeList,
-                {
-                    value: 'code_cd',
-                    text : 'code_name'
-                },
-                '#selPosition',
-                '전체'
-            )
-        }
-
-        /**
-         * empList.jsp 테이블에서 선택한 행 데이터를 화면에 보여줍니다. (select 에 들어가는 데이터 제외)
-         *
-         * @param {string} formId form 아이디
-         * @param {object} rowData 이전화면에서 선택한 row 데이터
-         * */
-        ctrl.setInputView = function (formId, rowData) {
-            var self = et.vc
-
-            let triggerElement = []
-
-            $(formId).find('input, select').each(function (index, element) {
-                const name = $(element).prop('name')
-                if ($(element).prop('localName') === 'select') {
-                    if (rowData[name] === undefined) {
-                        $(element).children('option:eq(0)').prop('selected', true)
-                    }
-                    else {
-                        $(element).val(rowData[name])
-                        triggerElement.push(element)
-                    }
-                }
-                else if ($(element).prop('localName') === 'input') {
-                    $(element).val(rowData[name] === undefined ? '' : rowData[name])
-                }
-            })
-
-            $.each(triggerElement, function (index, element) {
-                if ($(element).prop('localName') === 'select') {
-                    $(element).trigger('change')
-                }
-            })
-        }
-
-        // ============================== 동작 컨트롤 ==============================
-
-        // ============================== 이벤트 리스너 ==============================
-
-        /**
-         * 수정 버튼을 클릭했을 시, form 을 제출하는 함수를 호출합니다.
-         * */
-        ctrl.btnEditClickHandler = function () {
-            var self = et.vc
-
-            $('#editForm').submit()
-        }
-        // ============================== DataTables 생성, 이벤트들 ==============================
-
-        // ============================== Form 리스너 ==============================
-
-        /**
-         * form 데이터의 유효성을 검사합니다. 필수 조건 충족 여부, 길이 제한 등을 검사합니다.
-         *
-         * */
-        ctrl.validation = function () {
-            var self = et.vc
-
-            let editValidation
-                = new ETValidate('#editForm', self.path + '/update')
-                .setSubmitHandler(self.editSubmitHandler)
-                .setShowErrors(et.setErrorFunction())
-
-
-            // 커스텀 룰 - 핸드폰 형식 체크
-            ETValidate.addMethod('phoneReg', function (value, element, param) {
-                return self.regExp.phoneReg.test(value.toString().trim())
-            })
-            // 커스텀 룰 - 이메일 형식 체크
-            ETValidate.addMethod('emailReg', function (value, element, params) {
-                return self.regExp.emailReg.test(value.toString().trim())
-            })
-            // 커스텀 규칙 - 특수문자 체크
-            ETValidate.addMethod('irregularCharReg', function (value, element, params) {
-                return self.regExp.irregularCharReg.test(value.toString().trim())
-            })
-
-            // (1) emp_name
-            editValidation.validateRules(
-                'emp_name',
-                editValidation.REQUIRED,
-                '이름은 필수값 입니다.'
-            )
-            // (2) department
-            editValidation.validateRules(
-                'department',
-                editValidation.REQUIRED,
-                '부서는 필수값 입니다.'
-            )
-            // (3) position
-            editValidation.validateRules(
-                'position',
-                editValidation.REQUIRED,
-                '직위는 필수값 입니다.'
-            )
-
-            // (4) email
-            editValidation.validateRules(
-                'email_1',
-                'emailReg',
-                '메일은 필수값 입니다.'
-            )
-
-            // (5) phone_num
-            editValidation.validateRules(
-                'phone_num',
-                'phoneReg',
-                '유효한 전화번호가 아닙니다.'
-            )
-
-            editValidation.apply()
-        }
-
-        /**
-         * 유효성 검사를 통과한 form 데이터를 submit 합니다.
-         *
-         * @param {object} form HTMLFormElement
-         * */
-        ctrl.editSubmitHandler = function () {
-            var self = et.vc
-
-            let form = $('#editForm')
-            // form 데이터 직렬화
-            let formData = ETValidate.convertFormToObject(form, true, true)
-
-            console.log("[/sample/update] formData 확인", formData)
-
-            // new ETService()
-            //     .setSuccessFunction(self.editSuccessHandler)
-            //     .callService(self.path + "/update", formData);
-        }
-
-        /**
-         * form 제출 후, 서버에서 전송한 응답을 처리합니다.
-         *
-         * @param {object} result 서버 응답
-         * */
-        ctrl.editSuccessHandler = function (result) {
-            var self = et.vc
-
-        }
-
-        return ctrl
-    })
+	function (et, ctrl) {
+		if (_.isObject(et) && et.name === ETCONST.PROJECT_NAME) {
+			if (!et.vc || et.vc.name !== 'updateEmp') {
+				et.vc = ctrl(et)
+			}
+			
+		}
+		else {
+			console.error('ecoletree OR ETCONST is not valid. please check that common.js file was imported.')
+		}
+	}(this.ecoletree, function (et) {
+		
+		'use strict'
+		
+		var ctrl = {}
+		
+		ctrl.name = 'updateEmp'
+		ctrl.path = '/sample'
+		
+		// ============================== 화면 컨트롤 ==============================
+		/**
+		 * init VIEW
+		 */
+		ctrl.init = function (initData) {
+			var self = et.vc
+			
+			ctrl.initData = initData
+			ctrl.regExp = {}   // 정규식 패턴을 담고 있는 객체
+			
+			self.setRegExp()    // 정규식 등록
+			self.setSelect()    // select tag 데이터 입력
+			self.setInputView('#editForm', initData.rowData)    // form 데이터 집어넣기
+			self.validation()   // 유효성 검사
+			
+			$('#btnEdit').click(self.btnEditClickHandler)
+		}
+		
+		/**
+		 * 유효성 검사를 위한 정규식을 담고 있는 메서드
+		 * */
+		ctrl.setRegExp = function () {
+			var self = et.vc
+			
+			// 숫자만 (사번)
+			self.regExp['numberOnlyReg'] = /^\d+$/g
+			// 핸드폰 번호
+			self.regExp['phoneReg'] = /^\d{3}-\d{4}-\d{4}$/g
+			// 이메일 확인
+			self.regExp['emailReg'] = /^\w([-_.]?\w)*@\w([-_.]?\w)*\.[a-zA-Z]{2,3}$/i
+			// 빈칸 확인
+			self.regExp['whiteSpaceReg'] = /\s/g
+			// 특수문자 확인
+			self.regExp['irregularCharReg'] = /[!?@#$%^&*():;+-=~{}<>_[\]|\\"',./`₩]/g
+			// 하이픈(-) 제외 특수문자 확인
+			self.regExp['irregularCharRegWithoutHyphen'] = /[!?@#$%^&*():;+=~{}<>_[\]|\\"',./`₩]/g
+			// 하이픈(-), 닷(.) 제외 특수문자 확인
+			self.regExp['irregularCharRegWithoutHyphenAndDot'] = /[!?@#$%^&*():;+=~{}<>_[\]|\\"',/`₩]/g
+			// 영문이름 && 2~6 글자 제한
+			self.regExp['engOnlyReg'] = /^[a-zA-Z]?$/g
+			// 생년월일
+			// 1999.01.01, 1991.1.1, 1991-01-01, 1991-1-1
+			self.regExp['birthDateReg'] = /^\d{4}[.|-]\d{1,2}[.|-]\d{1,2}$/g
+		}
+		
+		/**
+		 * select 박스에 들어가는 데이터를 화면에 출력합니다.
+		 * */
+		ctrl.setSelect = function () {
+			var self = et.vc
+			
+			et.makeSelectOption(
+				self.initData.codeList,
+				{
+					value: 'code_cd',
+					text : 'code_name'
+				},
+				'#selPosition',
+				'전체'
+			)
+		}
+		
+		/**
+		 * empList.jsp 테이블에서 선택한 행 데이터를 화면에 출력합니다. (select 에 들어가는 데이터 제외)
+		 *
+		 * @param {string} formId form 아이디
+		 * @param {object} rowData 이전화면에서 선택한 row 데이터
+		 * */
+		ctrl.setInputView = function (formId, rowData) {
+			var self = et.vc
+			
+			let triggerElement = []
+			
+			$(formId).find('input, select').each(function (index, element) {
+				const name = $(element).prop('name')
+				if ($(element).prop('localName') === 'select') {
+					if (rowData[name] === undefined) {
+						$(element).children('option:eq(0)').prop('selected', true)
+					}
+					else {
+						$(element).val(rowData[name])
+						triggerElement.push(element)
+					}
+				}
+				else if ($(element).prop('localName') === 'input') {
+					$(element).val(rowData[name] === undefined ? '' : rowData[name])
+				}
+			})
+			
+			$.each(triggerElement, function (index, element) {
+				if ($(element).prop('localName') === 'select') {
+					$(element).trigger('change')
+				}
+			})
+		}
+		
+		// ============================== 동작 컨트롤 ==============================
+		
+		// ============================== 이벤트 리스너 ==============================
+		
+		/**
+		 * 수정 버튼을 클릭했을 시, form 을 제출하는 함수를 호출합니다.
+		 * */
+		ctrl.btnEditClickHandler = function () {
+			var self = et.vc
+			
+			$('#editForm').submit()
+		}
+		// ============================== DataTables 생성, 이벤트들 ==============================
+		
+		// ============================== Form 리스너 ==============================
+		
+		/**
+		 * form 데이터의 유효성을 검사합니다. 필수 조건 충족 여부, 길이 제한 등을 검사합니다.
+		 *
+		 * */
+		ctrl.validation = function () {
+			var self = et.vc
+			
+			let editValidation
+				= new ETValidate('#editForm', self.path + '/update')
+			.setSubmitHandler(self.editSubmitHandler)
+			.setShowErrors(et.setErrorFunction())
+			
+			// 커스텀 룰 - 숫자만 (사번)
+			ETValidate.addMethod('numberOnlyReg', function (value, element, params) {
+				return self.regExp.numberOnlyReg.test(value.toString().trim()) && !self.regExp.irregularCharReg.test(value.toString().trim())
+			})
+			// 커스텀 룰 - 핸드폰 형식 체크
+			ETValidate.addMethod('phoneReg', function (value, element, param) {
+				return self.regExp.phoneReg.test(value.toString().trim()) && !self.regExp.irregularCharRegWithoutHyphen.test(value.toString().trim())
+			})
+			// 커스텀 룰 - 이메일 형식 체크
+			ETValidate.addMethod('emailReg', function (value, element, params) {
+				return self.regExp.emailReg.test(value.toString().trim())
+			})
+			// 커스텀 룰 - 생년월일 체크
+			ETValidate.addMethod('birthDateReg', function (value, element, params) {
+				return self.regExp.birthDateReg.test(value.toString().trim()) && !self.regExp.irregularCharRegWithoutHyphenAndDot.test(value.toString().trim())
+			})
+			// 커스텀 룰 - 영문이름 체크
+			ETValidate.addMethod('engOnlyReg', function (value, element, params) {
+				return self.regExp.engOnlyReg.test(value.toString().trim()) && !self.regExp.irregularCharReg.test(value.toString().trim())
+			})
+			
+			// (1) emp_name
+			editValidation.validateRules(
+				'emp_name',
+				editValidation.REQUIRED,
+				'이름은 필수값 입니다.'
+			)
+			// (2) department
+			editValidation.validateRules(
+				'department',
+				editValidation.REQUIRED,
+				'부서는 필수값 입니다.'
+			)
+			// (3) position
+			editValidation.validateRules(
+				'position',
+				editValidation.REQUIRED,
+				'직위는 필수값 입니다.'
+			)
+			
+			// (4) email
+			editValidation.validateRules(
+				'email_1',
+				'emailReg',
+				'메일은 필수값 입니다.'
+			)
+			
+			// (5) phone_num
+			editValidation.validateRules(
+				'phone_num',
+				'phoneReg',
+				'유효한 전화번호가 아닙니다.'
+			)
+			
+			// (6) emp_engname
+			editValidation.validateRules(
+				'emp_engname',
+				'engOnlyReg',
+				'(영문이름) 영문만 입력해주세요'
+			)
+			
+			editValidation.apply()
+		}
+		
+		/**
+		 * 유효성 검사를 통과한 form 데이터를 submit 합니다.
+		 *
+		 * */
+		ctrl.editSubmitHandler = function () {
+			var self = et.vc
+			
+			let form = $('#editForm')
+			// form 데이터 직렬화
+			let formData = ETValidate.convertFormToObject(form, true, true)
+			
+			console.log('[/sample/update] formData 확인', formData)
+			
+			// new ETService()
+			//     .setSuccessFunction(self.editSuccessHandler)
+			//     .callService(self.path + "/update", formData);
+		}
+		
+		/**
+		 * form 제출 후, 서버에서 전송한 응답을 처리합니다.
+		 *
+		 * @param {object} result 서버 응답
+		 * */
+		ctrl.editSuccessHandler = function (result) {
+			var self = et.vc
+			
+			// TBD
+		}
+		
+		return ctrl
+	})
 )
