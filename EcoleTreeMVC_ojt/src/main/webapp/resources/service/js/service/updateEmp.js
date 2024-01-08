6fc7091a287369764be71b2486bfd86913f8824e
SeungwooJung99 <glenn@ecoletree.com>
2024-01-08 16:15:47 +0900

    fix: 날짜 validation 리턴 값 수정

--- EcoleTreeMVC_ojt/src/main/webapp/resources/service/js/service/updateEmp.js
+++ EcoleTreeMVC_ojt/src/main/webapp/resources/service/js/service/updateEmp.js
@@ -94,10 +94,8 @@
 		});
 		
 		ETValidate.addMethod("validBirthDayFormat", function(value, element, params) {
-			var regex = RegExp(/^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/);
-		    if ( !regex.test(value)) {
-       			 return false;
-    		}
+			var regex = /(^\d{4})-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
+		    return regex.test(value);
 		});
 		
 		ETValidate.addMethod("twoNumber", function(value, element, params){
